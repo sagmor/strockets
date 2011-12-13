a344bde7ca033b34f79abc1ce53902dc730b9da0
Sebastian Gamboa <me@sagmor.com>
2011-12-13 19:21:40 -0300

    Add flag to stitch all files

--- lib/strockets.rb
+++ lib/strockets.rb
@@ -5,6 +5,11 @@ require "strockets/bundle"
 module Strockets
   def self.enable(environment,options={})
     environment.register_bundle_processor 'application/javascript', Bundle
-    environment.register_engine '.stitch', Processor
+
+    if options[:stitch_all]
+      environment.register_processor 'application/javascript', Processor
+    else
+      environment.register_engine '.stitch', Processor
+    end
   end
 end
